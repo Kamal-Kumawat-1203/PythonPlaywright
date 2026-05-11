38808f8e1f9207deb9f1f3a59fe8671e7e61f4c0
Kamal Kumar Kumawat <kamalkumawat1203@gmail.com>
2026-05-11 17:05:02 +0530

    Add Playwright fixtures for video recording and tracing in config.py file

---
+++ Video_Recoring_and_Trace_Generateing.py
@@ -0,0 +1,30 @@
+import pytest
+from playwright.sync_api import sync_playwright
+
+@pytest.fixture(scope="function")
+def browser():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        yield browser
+        browser.close()
+
+@pytest.fixture(scope="function")
+def page(browser):
+    # context = browser.new_context()
+
+    # # If video recording is not required then comment the line below and uncomment the upper line
+
+    context = browser.new_context(
+        record_video_dir="c:/Users/kumaw/PycharmProjects/PlaywrightLearning/testcases/videos/",
+        record_video_size={"width": 1920, "height": 1080})  # record_video_size={"width": 3840, "height": 2160}
+
+    # Start tracing before creating/navigating a page.
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+
+    page = context.new_page()
+    page.set_viewport_size({"width": 1425, "height": 760})
+    yield page
+    # Stop tracing and export it into a zip archive.
+    context.tracing.stop(path="c:/Users/kumaw/PycharmProjects/PlaywrightLearning/testcases/trace/trace.zip")
+    page.close()
+    context.close()
