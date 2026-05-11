2e315486876ada2f27649d063b6b559aabcf1167
Kamal Kumar Kumawat <kamalkumawat1203@gmail.com>
2026-05-11 15:49:29 +0530

    Clicking right on the page and selcting on opened menu item

---
+++ Right_Click_then_Select_Menu_Item.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import Page
+
+def test_right_click_then_select_menu_item(page):
+    page.goto("https://deluxe-menu.com/popup-mode-sample.html")
+    page.wait_for_timeout(1500)
+    page.locator("//html/body/div[1]/table/tbody/tr/td[2]/div[2]/table[1]/tbody/tr/td[3]/p[2]/img").click(button="right")
+    page.wait_for_timeout(1000)
+    page.locator("//*[@id='dm2m1i1tdT']").hover()
+    page.wait_for_timeout(1000)
+    page.locator("//*[@id='dm2m2i1tdT']").hover()
+    page.wait_for_timeout(1000)
+    # In the below hover, we can not directly click on the menu list first, we have to hover on the item
+    page.locator("//*[@id='dm2m3i1tdT']").hover()
+    page.wait_for_timeout(200)
+    page.locator("//*[@id='dm2m3i1tdT']").click()
+    page.wait_for_timeout(3000)
